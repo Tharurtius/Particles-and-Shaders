4c04d83625f0b57ec5f835bd6f322bc09de64546
Tharurtius <michael.yee6@studytafensw.edu.au>
2022-10-10 23:25:50 +1100

    feedback pt 2

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log($"Missile did {Random.Range(1, 5)} damage.");
+    }
+}
